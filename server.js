90b10cea8da384e76065367958d2a6ab7d92f955
Nico Aparicio <nicolas.matias.aparicio@gmail.com>
2022-09-01 23:34:37 -0300

    proyecto servidor con rutas get

--- server.js
+++ server.js
@@ -7,16 +7,25 @@ app.use(express.json());
 
 const DB = new db();
 
-console.log(db)
 app.get('/productos', async (req, res) => {
-    const data 
-    res.send({ error: false });
+    const data = await DB.getAll();
+    res.send(data);
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const longitudObjetos = (await DB.getAll()).length;
+    const { id } = Math.random()  * (longitudObjetos - 1) + 1;
+    try{
+        const data = await DB.getById(id);
+        return res.send(data);
+    }
+    catch(e){
+        return res.status(404).send({ error: true, msg: e.message })
+    }
 });
 
 
 
-
-
 app.listen(8080, () => {
-    console.log("Servidor listo para escuchar")
+    console.log("Servidor listo")
 })
