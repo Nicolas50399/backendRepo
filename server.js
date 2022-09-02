8647dd9d13563658b16c0e82c6151b686cee4c39
Nico Aparicio <nicolas.matias.aparicio@gmail.com>
2022-09-01 22:49:03 -0300

    primer commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
 const app = express();
 const db = require("./main.js")
 
+app.use(express.urlencoded());
+app.use(express.json());
+
+const DB = new db();
+
 console.log(db)
-app.get('/', (req, res) => {
+app.get('/productos', async (req, res) => {
+    const data 
     res.send({ error: false });
 });
 
