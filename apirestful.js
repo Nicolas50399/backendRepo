7d82179b9c253eb01340d32a67c8b5b7ce7ece4c
Nico Aparicio <nicolas.matias.aparicio@gmail.com>
2022-09-17 00:48:31 -0300

    handlebars prductos

--- apirestful.js
+++ apirestful.js
@@ -6,10 +6,85 @@ const { Router } = express;
 
 const router = Router();
 
-router.use(express.urlencoded());
+router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 const DB = new db();
+
+app.set('views', './views');
+
+app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
+
+/*
+app.get('/indexPug', (req, res) => {
+    res.render('indexPug.pug', { titulo: 'Usando Pug JS en Express' });
+});
+
+app.get('/indexPug', (req, res) => {
+    res.send('Hola mundo');
+})
+app.get('/urlparam', (req, res) => {
+    res.send(req.query);
+})
+app.post('/urljson', (req, res) => {
+    res.send(req.body);
+})
+
+
+
+
+app.get('/', (req, res) => {
+    var mascots = [
+        { name: 'Sammy', organization: 'DigitalOcean', birth_year: 2012 },
+        { name: 'Tux', organization: 'Linux', birth_year: 1996 },
+        { name: 'Moby Dock', organization: 'Docker', birth_year: 2013 }
+    ]
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/indexPug', {
+        mascots: mascots,
+        tagline: tagline
+    });
+});*/
+
+app.get('./indexEjs', (req, res) => {
+    res.render('pages/indexEjs');
+})
+
+
+const bcrypt = require("bcrypt");
+const handlebars = require("express-handlebars");
+
+
+// * ---------------------FRONTEND-------------------------------------
+
+app.get("/agregarProductos", (req, res) => {
+    res.render("indexHbs", { layout: "agregarProductos" }); //*EN HANDLEBARS
+    res.render("indexPug");//*EN PUG
+    res.render('pages/indexEjs');//*EN EJS
+
+});
+app.get("/admin", async (req, res) => {
+    const productos = await DB.getAll();
+    res.render("indexHbs", { layout: "productos", productos });//*EN HANDLEBARS
+});
+
+app.get("/producto/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await DB.getById(id);
+        res.render("indexHbs", { layout: "producto", ...data });//*EN HANDLEBARS
+    } catch (e) {
+        return res.status(404).render("indexHbs", { layout: "error" });//*EN HANDLEBARS
+    }
+
+    res.render("indexHbs", { layout: "productos", productos });//*EN HANDLEBARS
+});
+
+//*-------------------------REQUEST---------------------------------------
+
 
 router.get('/', async (req, res) =>{
     const data = await DB.getAll();
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//const db = require("./main.js")
+const db = require("./main.js")
 const productosRouter = require('./apirestful')
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +10,7 @@ app.use('/api/productos', productosRouter)
 
 app.use("/", express.static(__dirname + "/public"));
 
-/*
+
 const DB = new db();
 
 app.get('/productos', async (req, res) => {
@@ -20,54 +20,17 @@ app.get('/productos', async (req, res) => {
 
 app.get('/productoRandom', async (req, res) => {
     const longitudObjetos = (await DB.getAll()).length;
-    const { id } = Math.random()  * (longitudObjetos - 1) + 1;
-    try{
+    const { id } = Math.random() * (longitudObjetos - 1) + 1;
+    try {
         const data = await DB.getById(id);
         return res.send(data);
     }
-    catch(e){
+    catch (e) {
         return res.status(404).send({ error: true, msg: e.message })
     }
 });
-*/
-
-app.set('views', './views');
-app.set('view engine', 'pug');
-
-app.get('/index', (req, res) => {
-    res.render('index.pug', {titulo: 'Usando Pug JS en Express'});
-});
-
-app.get('/index', (req, res) => {
-    res.send('Hola mundo');
-})
-app.get('/urlparam', (req, res) => {
-    res.send(req.query);
-})
-app.post('/urljson', (req, res) => {
-    res.send(req.body);
-})
 
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    var mascots = [
-        { name: 'Sammy', organization: 'DigitalOcean', birth_year: 2012},
-        { name: 'Tux', organization: 'Linux', birth_year: 1996},
-        { name: 'Moby Dock', organization: 'Docker', birth_year: 2013}
-    ]
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-
-    res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline
-    });
-});
-
-app.get('./about', (req, res) => {
-    res.render('pages/about');
-})
 
 
 app.listen(8080, () => {
