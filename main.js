0d6c11312da6cc65a819bde22bb12fb61c4921fc
Nico Aparicio <nicolas.matias.aparicio@gmail.com>
2022-10-20 02:18:40 -0300

    seteo timestamp productos

--- main.js
+++ main.js
@@ -1,116 +1,134 @@
 const fs = require('fs');
 
 class Contenedor {
-    constructor(nameFile){
+    constructor(nameFile) {
         this.file = nameFile;
     }
 
-    async createFile(nameFile){//Crea el archivo, inicialmente vacio
+    async createFile(nameFile) {//Crea el archivo, inicialmente vacio
         try {
             await fs.promises.writeFile(`${nameFile}`, '[]');
             console.log(`Archivo ${nameFile} creado exitosamente`);
         }
-        catch(e){
+        catch (e) {
             console.log('no se pudo crear el archivo');
         }
     }
 
-    async save(object, nameFile){//Agrega un objeto al array del archivo
+    async save(object, nameFile) {//Agrega un objeto al array del archivo
         const data = await fs.promises.readFile(
             `${nameFile}`, 'utf-8'
-           );
+        );
         const elements = JSON.parse(data);
         object.id = elements.length + 1;
-        if(nameFile == 'carrito.txt'){
-            object.products = [];
+
+        if (elements.some(e => e.id == object.id)) {
+            object.id = elements[elements.length - 1].id + 1;//Reasigno id para que no se repita
         }
 
-        if(elements.some(e => e.id == object.id)){
-            object.id = elements[elements.length-1].id + 1;
+        switch (nameFile) {
+            case 'carrito.txt': {
+                object.products = [];
+                elements.push(object, nameFile);
+                break;
+            }
+            case 'productos.txt': {
+                //Si ya se repite el producto, se actualiza stock y cantidad
+                //! (No se va a repetir si todos los ids van a ser distintos)
+                if (elements.some(e => e == object)) {
+                    const product = elements.find(e => e.id == object.id)
+                    product.stock += object.stock;
+                    product.timeStamp = Date.now();
+                }
+                else{
+                    object.timeStamp = Date.now();
+                    elements.push(object, nameFile);
+                }
+                break;
+            }
+            default: break;
         }
         
-        
-        elements.push(object, nameFile);
         const elementString = JSON.stringify(elements);
 
-        try{
+        try {
             await fs.promises.writeFile(
-                `${nameFile}`,`${elementString}`
+                `${nameFile}`, `${elementString}`
             )
             console.log(`Elemento de ${nameFile} guardado exitosamente`);
         }
-        catch(e){
+        catch (e) {
             console.log('No se pudo guardar al producto en el archivo');
         }
 
         return object.id;
     }
 
-    async getById(id, nameFile){//Devuelve un producto mostrandolo por consola
+    async getById(id, nameFile) {//Devuelve un producto mostrandolo por consola
         const data = await fs.promises.readFile(
             `${nameFile}`, 'utf-8'
-           );
+        );
         const productos = JSON.parse(data);
-        
+
 
         const productoBuscado = productos.find((unProducto) => unProducto.id == id)
-        if(productoBuscado){
+        if (productoBuscado) {
             console.log(productoBuscado);
             return productoBuscado;
         }
-        else{
+        else {
             const noEncontrado = "Elemento no encontrado"
             console.log(noEncontrado);
             return noEncontrado;
         }
     }
 
-    async getAll(nameFile){//Devuelve contenido el archivo mostrandolo por consola
-        try{
+    async getAll(nameFile) {//Devuelve contenido el archivo mostrandolo por consola
+        try {
             const data = await fs.promises.readFile(
                 `${nameFile}`, 'utf-8'
-               );
+            );
             const personas = JSON.parse(data);
             console.log(personas);
             return personas;
         }
-        catch(e){
+        catch (e) {
             const noEncontrado = "No encontrado"
             console.log(noEncontrado);
             return noEncontrado;
         }
     }
 
-    async deleteById(id, nameFile){//Quita el producto del array en el archivo
+    async deleteById(id, nameFile) {//Quita el producto del array en el archivo
         const data = await fs.promises.readFile(
             `${nameFile}`, 'utf-8'
-           );
+        );
         const personas = JSON.parse(data);
 
-        if(personas.some(unaPersona => unaPersona.id == id)){
+        if (personas.some(unaPersona => unaPersona.id == id)) {
             const personasActualizadas = JSON.stringify(personas.filter(unaPersona => unaPersona.id != id))
-            try{
+            try {
                 await fs.promises.writeFile(
                     `${nameFile}`, `${personasActualizadas}`
                 )
                 console.log(`Item de ${nameFile} borrado exitosamente`)
             }
-            catch(e){
+            catch (e) {
                 console.log('No se pudo borrar al producto')
             }
         }
-        else{
+        else {
             console.log('No se encuentra en el archivo')
         }
 
     }
 
-    async deleteAll(nameFile){//Vacia el array de productos en el archivo
-        try{
+    async deleteAll(nameFile) {//Vacia el array de productos en el archivo
+        try {
             await fs.promises.writeFile(`${nameFile}`, '[]')
             console.log(`Contenido total de ${nameFile} borrado exitosamente`)
         }
-        catch(e){
+        catch (e) {
             console.log('No se pudo borrar')
         }
     }
